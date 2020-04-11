6514a3e7ed93ca272335561ceb98ee66813e095f
Angel Contreras <angel.contreras23@me.com>
2020-04-10 23:39:38 -0700

    Centralized repeated code

---
+++ calendars/full-calendar.jsx
@@ -0,0 +1,49 @@
+import { css } from 'uebersicht';
+import {
+  className,
+  months,
+  refreshFrequency,
+  parser,
+  yearComponent,
+  monthComponent,
+} from '../lib/calendars/commons';
+
+export const containerCss = css`
+    background: rgba(0, 0, 0, 0.3);
+    padding: 50px 50px 30px 50px;
+    border-radius: 20px;
+    border-style: outset;
+    border-width: 1px;
+`
+export const calendarBody = css`
+    display: flex;
+    width: 525px;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+`;
+
+export const yearCss = css`
+    font-size: 90px;
+    margin-bottom: 10px;
+`;
+
+const commandBuilder = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    return months.reduce( (p, c) => p + `cal -h ${c} ${year}; echo "|";`, '');
+};
+
+export const command = commandBuilder();
+
+export const render = ({ output, error }) => {
+    const o = parser(output);
+    return o && o.length ? <div className={containerCss}>
+        { yearComponent(yearCss) }
+        <div className={calendarBody} >
+            {o.map(m => m ? monthComponent(m) : null)}
+        </div>
+    </div> : null;
+};
+
+export { refreshFrequency, className }
---
+++ calendars/three-month-calendar.jsx
@@ -0,0 +1,59 @@
+import { css } from 'uebersicht';
+import {
+  className,
+  months,
+  refreshFrequency,
+  parser,
+  yearComponent,
+  monthComponent,
+} from '../lib/calendars/commons';
+
+export const containerCss = css`
+    background: rgba(0, 0, 0, 0.3);
+    padding: 25px;
+    border-radius: 20px;
+    border-style: outset;
+    border-width: 1px;
+    width: 900px;
+    
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`
+export const calendarBody = css`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    flex-grow: 1;
+    margin-left: 10px;
+`;
+
+export const yearCss = css`
+    font-size: 56px;
+    width:60px;
+    word-wrap: break-word;
+    line-height: 90%
+    
+`;
+
+const commandBuilder = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const monthIndex = date.getMonth();
+    const lastMonthAndAfter = months.slice(monthIndex - 1, monthIndex + 2);
+    return lastMonthAndAfter.reduce( (p, c) => p + `cal -h ${c} ${year}; echo "|";`, '');
+};
+
+export const command = commandBuilder();
+
+export const render = ({ output }) => {
+    const o = parser(output);
+    return o && o.length ? <div className={containerCss}>
+            { yearComponent(yearCss) }
+        <div className={calendarBody} >
+            { o.map(m => m ? monthComponent(m) : null) }
+        </div>
+    </div> : null;
+};
+
+export { refreshFrequency, className }
